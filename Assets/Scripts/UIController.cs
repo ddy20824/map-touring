dc404243287542b36abddeabd373e2050bba7054
Chiayi <angel05446a@gmail.com>
2024-11-04 15:13:13 -0500

    Add level2 mapPuzzle control

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
@@ -10,41 +6,42 @@ public class UIController : MonoBehaviour
     public GameObject highlight;
     public GameObject player;
     public GameObject[] Map;
-    public int mapCount;
+    public GameObject[] mapPuzzle;
+    public int level;
 
-    GameObject[] mapPuzzle_Level1;
-    float[] map_XPosition_Level1;
+    Vector2[] map_Position;
+    Vector2[] mapPuzzle_Position;
+    int mapCount;
     public bool isMapOpen = false;
     int currentIndexOfMap = 0;
     int selectedMapIndex = -1;
     float count = 1.0f;
-    // Start is called before the first frame update
     void Start()
     {
-        closePanel();
+        CloseMapPuzzle();
         GetMapInfo();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
             if (isMapOpen)
             {
-                closePanel();
+                CloseMapPuzzle();
             }
             else
             {
                 OpenMapPuzzle();
             }
         }
+
         if (Input.GetKeyDown(KeyCode.Return) && isMapOpen)
         {
             if (selectedMapIndex == -1)
             {
                 selectedMapIndex = currentIndexOfMap;
-                mapPuzzle_Level1[selectedMapIndex].transform.GetChild(0).gameObject.SetActive(true);
+                mapPuzzle[selectedMapIndex].transform.GetChild(0).gameObject.SetActive(true);
             }
             else
             {
@@ -52,46 +49,17 @@ public class UIController : MonoBehaviour
                 ExchangeMap();
             }
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+
+        if (level == 1)
         {
-            if (currentIndexOfMap != 0)
-            {
-                var expectIndex = currentIndexOfMap - 1;
-                if (expectIndex == selectedMapIndex)
-                {
-                    if (expectIndex != 0)
-                    {
-                        expectIndex--;
-                    }
-                    else
-                    {
-                        expectIndex++;
-                    }
-                }
-                currentIndexOfMap = expectIndex;
-                highlight.transform.position = mapPuzzle_Level1[currentIndexOfMap].transform.position;
-            }
+            Level1Control();
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        else
         {
-            if (currentIndexOfMap < mapCount - 1)
-            {
-                var expectIndex = currentIndexOfMap + 1;
-                if (expectIndex == selectedMapIndex)
-                {
-                    if (expectIndex < mapCount - 1)
-                    {
-                        expectIndex++;
-                    }
-                    else
-                    {
-                        expectIndex--;
-                    }
-                }
-                currentIndexOfMap = expectIndex;
-                highlight.transform.position = mapPuzzle_Level1[currentIndexOfMap].transform.position;
-            }
+            Level2Control();
         }
+
+        highlight.transform.position = mapPuzzle[currentIndexOfMap].transform.position;
         // if (count < 1.0f)
         // {
         //     var point1 = mapPuzzle_Level1[selectedMapIndex].transform.position;
@@ -111,54 +79,153 @@ public class UIController : MonoBehaviour
         SetCurrentMapIndex();
     }
 
+    void CloseMapPuzzle()
+    {
+        panel.SetActive(false);
+        isMapOpen = false;
+        selectedMapIndex = -1;
+    }
+
     void GetMapInfo()
     {
-        var map = panel.transform.GetChild(0).gameObject;
-        mapPuzzle_Level1 = new GameObject[mapCount];
-        map_XPosition_Level1 = new float[mapCount];
+        mapCount = Map.Length;
+        map_Position = new Vector2[mapCount];
+        mapPuzzle_Position = new Vector2[mapCount];
         for (int i = 0; i < mapCount; i++)
         {
-            mapPuzzle_Level1[i] = map.transform.GetChild(i).gameObject;
-            map_XPosition_Level1[i] = Map[i].transform.position.x;
+            map_Position[i] = Map[i].transform.position;
+            mapPuzzle_Position[i] = mapPuzzle[i].transform.position;
         }
     }
 
     void SetCurrentMapIndex()
     {
         var level1XPosition = new float[3] { -14, 13, 39 };
-        var playerPosition = player.transform.position.x;
-        for (int i = 0; i < mapCount; i++)
+        if (level == 1)
         {
-            if (playerPosition < level1XPosition[i])
+            var playerPosition = player.transform.position.x;
+            for (int i = 0; i < mapCount; i++)
             {
-                currentIndexOfMap = i;
-                highlight.transform.position = mapPuzzle_Level1[currentIndexOfMap].transform.position;
-                break;
+                if (playerPosition < level1XPosition[i])
+                {
+                    currentIndexOfMap = i;
+                    break;
+                }
+            }
+
+        }
+        else
+        {
+            var playerPosition = player.transform.position;
+            if (playerPosition.x < 13)
+            {
+                currentIndexOfMap = playerPosition.y > -7 ? 0 : 2;
+            }
+            else
+            {
+                currentIndexOfMap = playerPosition.y > -7 ? 1 : 3;
             }
         }
     }
 
     void ExchangeMapPuzzle()
     {
-        mapPuzzle_Level1[selectedMapIndex].transform.GetChild(0).gameObject.SetActive(false);
-        var tempPosition = mapPuzzle_Level1[selectedMapIndex].transform.position;
-        var selected = mapPuzzle_Level1[selectedMapIndex];
-        var current = mapPuzzle_Level1[currentIndexOfMap];
-        selected.transform.position = mapPuzzle_Level1[currentIndexOfMap].transform.position;
+        mapPuzzle[selectedMapIndex].transform.GetChild(0).gameObject.SetActive(false);
+        var tempPosition = mapPuzzle[selectedMapIndex].transform.position;
+        var selected = mapPuzzle[selectedMapIndex];
+        var current = mapPuzzle[currentIndexOfMap];
+        selected.transform.position = mapPuzzle[currentIndexOfMap].transform.position;
         current.transform.position = tempPosition;
-        mapPuzzle_Level1[selectedMapIndex] = current;
-        mapPuzzle_Level1[currentIndexOfMap] = selected;
+        mapPuzzle[selectedMapIndex] = current;
+        mapPuzzle[currentIndexOfMap] = selected;
         selectedMapIndex = -1;
         // count = 0.0f;
     }
 
     void ExchangeMap()
     {
-        for (int i = 0; i < mapPuzzle_Level1.Length; i++)
+        for (int i = 0; i < mapCount; i++)
         {
-            var name = mapPuzzle_Level1[i].name;
+            var name = mapPuzzle[i].name;
             var index = int.Parse(name.Substring(name.Length - 1)) - 1;
-            Map[index].transform.position = new Vector2(map_XPosition_Level1[i], Map[index].transform.position.y);
+            Map[index].transform.position = map_Position[i];
+        }
+    }
+
+    void Level2Control()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentIndexOfMap % 2 != 0)
+            {
+                currentIndexOfMap--;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentIndexOfMap % 2 == 0)
+            {
+                currentIndexOfMap++;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentIndexOfMap < 4 && currentIndexOfMap > 1)
+            {
+                currentIndexOfMap -= 2;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentIndexOfMap >= 0 && currentIndexOfMap < 2)
+            {
+                currentIndexOfMap += 2;
+            }
+        }
+        // highlight.transform.position = mapPuzzle[currentIndexOfMap].transform.position;
+    }
+    void Level1Control()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentIndexOfMap != 0)
+            {
+                var expectIndex = currentIndexOfMap - 1;
+                if (expectIndex == selectedMapIndex)
+                {
+                    if (expectIndex != 0)
+                    {
+                        expectIndex--;
+                    }
+                    else
+                    {
+                        expectIndex++;
+                    }
+                }
+                currentIndexOfMap = expectIndex;
+                // highlight.transform.position = mapPuzzle[currentIndexOfMap].transform.position;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentIndexOfMap < mapCount - 1)
+            {
+                var expectIndex = currentIndexOfMap + 1;
+                if (expectIndex == selectedMapIndex)
+                {
+                    if (expectIndex < mapCount - 1)
+                    {
+                        expectIndex++;
+                    }
+                    else
+                    {
+                        expectIndex--;
+                    }
+                }
+                currentIndexOfMap = expectIndex;
+                // highlight.transform.position = mapPuzzle[currentIndexOfMap].transform.position;
+            }
         }
     }
 
@@ -178,12 +245,5 @@ public class UIController : MonoBehaviour
             Vector3 m4 = Vector3.Lerp(controlPoint2, startingPoint, count);
             gameObject2.transform.position = Vector3.Lerp(m3, m4, count);
         }
-    }
-
-    void closePanel()
-    {
-        panel.SetActive(false);
-        isMapOpen = false;
-        selectedMapIndex = -1;
     }
 }
