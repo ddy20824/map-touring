23d520f195dd665b38d2fbb5bb5f883ebf58bf98
Chiayi <angel05446a@gmail.com>
2024-12-01 16:11:37 -0500

    Monster moving smooooooooth

---
+++ Assets/Scripts/BezierFollow.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] routes;
+
+    private int routeToGo;
+
+    private float tParam;
+
+    private Vector2 objectPosition;
+
+    private float speedModifier;
+
+    private bool coroutineAllowed;
+    Animator anim;
+    int direction = -1;
+    bool isAttacking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("IsRun", true);
+        routeToGo = 0;
+        tParam = 0f;
+        speedModifier = 0.2f;
+        coroutineAllowed = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coroutineAllowed && !isAttacking)
+        {
+            StartCoroutine(GoByTheRoute(routeToGo));
+        }
+    }
+
+    private IEnumerator GoByTheRoute(int routeNum)
+    {
+        if (!isAttacking)
+        {
+
+            direction = routeNum == 0 ? -1 : 1;
+            transform.localScale = new Vector3(direction * 5, 5, 5);
+            coroutineAllowed = false;
+
+            Vector2 p0 = routes[routeNum].GetChild(0).position;
+            Vector2 p1 = routes[routeNum].GetChild(1).position;
+            Vector2 p2 = routes[routeNum].GetChild(2).position;
+            Vector2 p3 = routes[routeNum].GetChild(3).position;
+
+            while (tParam < 1 && !isAttacking)
+            {
+                tParam += Time.deltaTime * speedModifier;
+
+                objectPosition = Mathf.Pow(1 - tParam, 3) * p0 + 3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 + 3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + Mathf.Pow(tParam, 3) * p3;
+
+                transform.position = objectPosition;
+                yield return new WaitForEndOfFrame();
+            }
+
+            if (!isAttacking) // Only reset parameters if not attacking
+            {
+                tParam = 0f;
+                routeToGo += 1;
+
+                if (routeToGo > routes.Length - 1)
+                {
+                    routeToGo = 0;
+                }
+
+            }
+            coroutineAllowed = true;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            direction = (transform.position.x > other.transform.position.x) ? 1 : -1;
+            transform.localScale = new Vector3(direction * 5, 5, 5);
+            GameState.Instance.SetPlayerFronze(true);
+            SetMonsterAttackAnimation();
+        }
+    }
+    void SetMonsterAttackAnimation()
+    {
+        anim.SetBool("IsAttack", true);
+        isAttacking = true;
+        StartCoroutine(Helper.Delay(StopMonsterAnimation, 0.5f));
+    }
+
+    void StopMonsterAnimation()
+    {
+        GameState.Instance.SetPlayerFronze(false);
+        anim.SetBool("IsAttack", false);
+        isAttacking = false;
+        GameState.Instance.SetPlayerDie(true);
+    }
+}
--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -3,67 +3,68 @@ using UnityEngine;
 public class MonsterController : MonoBehaviour
 {
     Animator anim;
-    bool isAutoMon;
+    // bool isAutoMon;
     bool isHiddenMon;
-    Vector3 position1;
-    Vector3 position2;
-    int direction = -1;
+    // Vector3 position1;
+    // Vector3 position2;
+    // int direction = -1;
     bool isAttacking = false;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-        isAutoMon = CompareTag("autoMon");
+        // isAutoMon = CompareTag("autoMon");
         isHiddenMon = CompareTag("hiddenMon");
-        if (isAutoMon)
-        {
-            anim.SetBool("IsRun", true);
-            position1 = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
-            position2 = new Vector3(transform.localPosition.x + 11, transform.localPosition.y, 0);
-        }
-        else if (isHiddenMon)
+        // if (isAutoMon)
+        // {
+        //     anim.SetBool("IsRun", true);
+        //     position1 = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
+        //     position2 = new Vector3(transform.localPosition.x + 11, transform.localPosition.y, 0);
+        // }
+        // else 
+        if (isHiddenMon)
         {
             StartCoroutine(Helper.Delay(SetMonsterIdleAnimation, 0.1f));
         }
     }
-    private float duration = 0;
+    // private float duration = 0;
     void Update()
     {
         //start -13, end -2
-        if (isAutoMon && !isAttacking)
-        {
-            position1.x = -13 + Helper.FindParentWithTag(this.gameObject, "map").transform.localPosition.x;
-            position2.x = -2 + Helper.FindParentWithTag(this.gameObject, "map").transform.localPosition.x;
-            if (direction == -1)
-            {
-                if (duration < 2.5)
-                {
-                    transform.localScale = new Vector3(direction * 5, 5, 5);
-                    transform.localPosition = Vector3.Lerp(transform.localPosition, position2, 0.005f);
-                    duration += Time.deltaTime;
+        // if (isAutoMon && !isAttacking)
+        // {
+        //     position1.x = -13 + Helper.FindParentWithTag(this.gameObject, "map").transform.localPosition.x;
+        //     position2.x = -2 + Helper.FindParentWithTag(this.gameObject, "map").transform.localPosition.x;
+        //     if (direction == -1)
+        //     {
+        //         if (duration < 2.5)
+        //         {
+        //             transform.localScale = new Vector3(direction * 5, 5, 5);
+        //             transform.localPosition = Vector3.Lerp(transform.localPosition, position2, 0.005f);
+        //             duration += Time.deltaTime;
 
-                }
-                else
-                {
-                    direction = 1;
-                    duration = 0;
-                }
-            }
-            else
-            {
-                if (duration < 2.5)
-                {
-                    transform.localScale = new Vector3(direction * 5, 5, 5);
-                    transform.localPosition = Vector3.Lerp(transform.localPosition, position1, 0.005f);
-                    duration += Time.deltaTime;
-                }
-                else
-                {
-                    direction = -1;
-                    duration = 0;
-                }
-            }
-        }
+        //         }
+        //         else
+        //         {
+        //             direction = 1;
+        //             duration = 0;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         if (duration < 2.5)
+        //         {
+        //             transform.localScale = new Vector3(direction * 5, 5, 5);
+        //             transform.localPosition = Vector3.Lerp(transform.localPosition, position1, 0.005f);
+        //             duration += Time.deltaTime;
+        //         }
+        //         else
+        //         {
+        //             direction = -1;
+        //             duration = 0;
+        //         }
+        //     }
+        // }
     }
 
     void SetMonsterIdleAnimation()
@@ -119,11 +120,11 @@ public class MonsterController : MonoBehaviour
         {
             if (other.gameObject.name == "Player")
             {
-                if (isAutoMon)
-                {
-                    direction = (this.transform.position.x > other.transform.position.x) ? 1 : -1;
-                    transform.localScale = new Vector3(direction * 5, 5, 5);
-                }
+                // if (isAutoMon)
+                // {
+                //     direction = (this.transform.position.x > other.transform.position.x) ? 1 : -1;
+                //     transform.localScale = new Vector3(direction * 5, 5, 5);
+                // }
                 GameState.Instance.SetPlayerFronze(true);
                 if (isHiddenMon)
                 {
